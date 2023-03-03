ce24cc46fe584a68f98ac92e0f60572e800ef52c
wesleyepeneto <wesleyepeneto@hotmail.com>
2023-03-03 20:26:31 -0300

    first commit

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,38 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>10 minutos</td>
+              <td>Há x tempo</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>10 minutos</td>
+              <td>Há x tempo</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
